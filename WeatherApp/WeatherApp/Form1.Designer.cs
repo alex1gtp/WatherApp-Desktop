718d57e928a73d0653a9a7b25269704a5faab9f7
elianamanescu <elianamanescu@gmail.com>
2017-04-03 17:29:12 +0300

    Importing WebServices GlobalWeather for deserialising

---
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WeatherApp
+{
+    partial class formWeather
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBoxCounties = new System.Windows.Forms.ComboBox();
+            this.comboBoxCities = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBoxWeatherDetails = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBoxCities);
+            this.groupBox1.Controls.Add(this.comboBoxCounties);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(495, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBoxWeatherDetails);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(495, 263);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // comboBoxCounties
+            // 
+            this.comboBoxCounties.FormattingEnabled = true;
+            this.comboBoxCounties.Location = new System.Drawing.Point(158, 20);
+            this.comboBoxCounties.Name = "comboBoxCounties";
+            this.comboBoxCounties.Size = new System.Drawing.Size(317, 21);
+            this.comboBoxCounties.TabIndex = 0;
+            // 
+            // comboBoxCities
+            // 
+            this.comboBoxCities.FormattingEnabled = true;
+            this.comboBoxCities.Location = new System.Drawing.Point(158, 64);
+            this.comboBoxCities.Name = "comboBoxCities";
+            this.comboBoxCities.Size = new System.Drawing.Size(317, 21);
+            this.comboBoxCities.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Countries";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cities";
+            // 
+            // richTextBoxWeatherDetails
+            // 
+            this.richTextBoxWeatherDetails.Location = new System.Drawing.Point(6, 19);
+            this.richTextBoxWeatherDetails.Name = "richTextBoxWeatherDetails";
+            this.richTextBoxWeatherDetails.Size = new System.Drawing.Size(482, 237);
+            this.richTextBoxWeatherDetails.TabIndex = 0;
+            this.richTextBoxWeatherDetails.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 393);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "WeatherApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxCities;
+        private System.Windows.Forms.ComboBox comboBoxCounties;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBoxWeatherDetails;
+    }
+}
+
---
+++ WeatherApp/WeatherApp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeatherApp.ServiceReference;
+
+namespace WeatherApp
+{
+    public partial class formWeather : Form
+    {
+        public formWeather()
+        {
+            InitializeComponent();
+
+            BasicHttpBinding binding = new BasicHttpBinding();
+            binding.MaxReceivedMessageSize = 200000000;
+
+            EndpointAddress address = new EndpointAddress("http://www.webservicex.com/globalweather.asmx");
+
+            GlobalWeatherSoapClient gwsc = new GlobalWeatherSoapClient(binding, address);
+
+            var cities = gwsc.GetCitiesByCountry("");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
